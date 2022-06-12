dd2895e3e5c1eabce5b3a6435130be7487d7fb5d
Your Name <github.com@landroverz.com>
2022-06-12 17:48:33 +0300

    removed path to node

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 LOGFILE="log/stream.log";
 ERRORFILE="log/err.log";
-screen -S now-playing bash -c "/opt/local/sbin/node/bin/node ./src/now-playing.js 1>>$LOGFILE 2>>$ERRORFILE"
+screen -S now-playing bash -c "node ./src/now-playing.js 1>>$LOGFILE 2>>$ERRORFILE"
