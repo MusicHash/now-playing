b3f00343365590ee9620afa986769e98db639465
Your Name <github.com@landroverz.com>
2022-06-19 11:18:53 +0300

    tests init

---
+++ tests/integration/spotify.spec.js
@@ -0,0 +1,16 @@
+import Spotify from '../../src/lib/providers/spotify.js';
+
+// 
+describe('Spotify', function() {
+
+
+    describe('a', function () {
+
+
+        it('b', async function () {
+
+        });
+
+
+    });
+});
