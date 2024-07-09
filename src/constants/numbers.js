cd0ff4c1f65b041ca3912d05d7cb0050a1cc7028
LandRover <github@landroverz.com>
2024-07-09 06:31:10 +0300

    numbers added

---
+++ src/constants/numbers.js
@@ -0,0 +1,12 @@
+/**
+ * System event consts
+ *
+ * @type {Object}
+ */
+const NUMBERS = {
+    CACHE_FOR_1_HOUR: 60 * 60,
+    CACHE_FOR_1_DAY: 60 * 60 * 24,
+    CACHE_FOR_180_DAYS: 60 * 60 * 24 * 180,
+};
+
+export { NUMBERS };
